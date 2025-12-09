f24c355c6101fdb5559c58f046d5b2d409484d2b
Haris367 <kharis963@gmail.com>
2025-12-10 02:35:21 +0500

    Variables.tf chaged

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,5 +14,12 @@ variable "repo_name" {
 }
 
 # VPC related
-variable "vpc_cidr" { type = string default = "10.0.0.0/16" }
-variable "public_subnets" { type = list(string) default = ["10.0.1.0/24","10.0.2.0/24"] }
+variable "vpc_cidr" { 
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnets" { 
+  type = list(string)
+  default = ["10.0.1.0/24","10.0.2.0/24"] 
+}
