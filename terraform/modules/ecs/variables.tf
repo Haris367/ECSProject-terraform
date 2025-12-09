145a7af5f5f306ea899c65383dcf3d260bca6df8
Haris367 <kharis963@gmail.com>
2025-12-10 02:27:05 +0500

    Multi-line block added

--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,7 +1,28 @@
-variable "cluster_name" { type = string }
-variable "repo_url" { type = string }
-variable "image_tag" { type = string }
-variable "vpc_id" { type = string }
-variable "public_subnets" { type = list(string) }
-variable "aws_account_id" { type = string }
-variable "aws_region" { type = string default = "us-east-1" }
+variable "cluster_name" {
+  type = string
+}
+
+variable "repo_url" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnets" {
+  type = list(string)
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
