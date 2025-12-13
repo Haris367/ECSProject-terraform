3773671d4479fda27538c981e8a883241a80a4e8
Haris367 <kharis963@gmail.com>
2025-12-13 21:06:29 +0500

    index.js message changed

--- app/index.js
+++ app/index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
  res.send({
     message: "NodeJS App deployed on ECS",
-    env: process.env.NODE_ENV || "not set",
-    tag: process.env.IMAGE_TAG || "not set"
+    // env: process.env.NODE_ENV || "not set",
+    // tag: process.env.IMAGE_TAG || "not set"
   });
 });
 
